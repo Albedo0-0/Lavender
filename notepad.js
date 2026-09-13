3d698a24997c14a5afb9a0f6d087b53d92681394
Albedo0-0 <theanji11@gmail.com>
2026-09-13 19:48:06 +0530

    Update notepad.js

--- notepad.js
+++ notepad.js
@@ -314,7 +314,7 @@ const Notepad = (function () {
 
       // ---- hanging-folder thin side panel (lives inside the composer modal) ----
 
-  const HANGING_PAGE_SIZE = 3;
+  const HANGING_PAGE_SIZE = 5;
 
   function hangingNoteRowHtml(n) {
     const label = n.type === 'checklist' ? ('(checklist) ' + (n.checklistItems || []).length + ' items') : n.text;
