245862cef744645d0833c7760fc5d8ad990ad070
Albedo0-0 <theanji11@gmail.com>
2026-09-15 18:00:50 +0530

    Update radio-data.js

--- radio-data.js
+++ radio-data.js
@@ -3,7 +3,7 @@
 const RadioData = (function () {
   const stations = [
     { id: 'vividh-bharati', name: 'Vividh Bharati', url: 'https://radio.wavespb.com/live/146ed6ec6dea5a24/146ed6ec6dea5a24.m3u8' },
-    { id: 'Akashvani pune', name: 'Pune', url: 'http://adio.wavespb.com/live/39d650ca0476a1fe/39d650ca0476a1fe.m3u8' },
+    { id: 'Akashvani pune', name: 'Pune', url: 'https://radio.wavespb.com/live/39d650ca0476a1fe/39d650ca0476a1fe.m3u8' },
     { id: 'Akashvani pune-fm', name: 'Pune FM', url: 'https://radio.wavespb.com/live/7b3a089ab67dcf3c/7b3a089ab67dcf3c.m3u8' },
   ];
 
